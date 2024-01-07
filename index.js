db156cce519c69de254569f26c5b1cb484e28a78
Saflin Hussain <saflin.ph@gmail.com>
2024-01-07 22:07:54 +0000

    add health check

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     console.log(req.headers);
     res.statusCode = 200;
-    res.end('<html><body><h1>Hello, World!</h1></body></html>');
+    res.end('<html><body><h1>Hello, World1!</h1></body></html>');
 })
 server.listen(port, hostname);
